f4007242fbdb591fc51bf766dbe737900bf1cc05
Brian Clinton <abc.florida@yahoo.com>
2017-04-25 06:11:16 -0400

    add wraprapp interface

---
+++ src/Interfaces/WraprApplicationInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Part of the BC Town <type here> extension.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Florida Web Design PSL License.
+ *
+ * This source file is subject to the Florida Web Design PSL License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    type here
+ * @version    1.0.1
+ * @author     Florida Web Design LLC
+ * @license    Florida Web Deisgn LLC PSL
+ * @copyright  (c) 2011-2016, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Platform;
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
