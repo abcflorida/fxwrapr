fe2aab4dd3d5a0100e050d146cc36dd92eeff57a
Brian Clinton <abc.florida@yahoo.com>
2017-04-25 06:29:25 -0400

    ff

---
+++ src/Interfaces/WraprApplicationInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Part of the BC Town <type here> extension.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Florida Web Design PSL License.
+ *
+ * This source file is subject to the Florida Web Design PSL License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    FxWrapr
+ * @version    1.0.1
+ * @author     Florida Web Design LLC
+ * @license    Florida Web Deisgn LLC PSL
+ * @copyright  (c) 2011-2016, Florida Web Deisgn LLC
+ * @link       http://flwebdesignservice.com
+ */
+
+namespace Abcflorida\Fxwrapr\Interfaces;
+
+Interface WraprApplicationInterface {
+    
+    /** Get the entire wrap 
+     * 
+     */
+    
+    /** Update one layer */
+    
+    /** Update a layer color 
+    */
+    
+}
+
